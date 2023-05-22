e252f633e241311505d36dadf962ba4a45a62cfd
crbraun <chris.r.braun@gmail.com>
2023-05-22 11:52:57 -0400

    Added AWS S3 dependency as we will be creating an S3 implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /**
  * iRacing ibt telemetry parser.
  */
+
 export { Telemetry } from './telemetry'
 export { TelemetrySample, TelemetryParamValue, TelemetrySampleProperty } from './telemetry-sample'
+
