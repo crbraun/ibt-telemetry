915ba3070ef02449bf017da3833d8fddb723b21a
crbraun <chris.r.braun@gmail.com>
2024-02-16 13:31:42 -0500

    Fixed other occurrence of using ascii instead of utf-8 for session info payload

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -30,7 +30,7 @@ const diskSubHeaderFromFileDescriptor = async (fd: number): Promise<DiskSubHeade
 
 const sessionInfoStringFromFileDescriptor = async (fd: number, telemetryHeader: TelemetryHeader): Promise<string> =>
   readFileToBuffer(fd, telemetryHeader.sessionInfoOffset, telemetryHeader.sessionInfoLength)
-    .then(x => x.toString('ascii'))
+    .then(x => x.toString('utf-8'))
 
 const varHeadersFromFileDescriptor = async (fd: number, telemetryHeader: TelemetryHeader): Promise<VarHeader[]> => {
   const numberOfVariables = telemetryHeader.numVars
